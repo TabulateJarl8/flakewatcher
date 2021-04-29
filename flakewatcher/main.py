fcb5356cdbcad2f4ce2f3acee8879c370be4ec71
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-04-29 17:52:35 -0400

    make dynamic updating in background work

--- flakewatcher/main.py
+++ flakewatcher/main.py
@@ -1,9 +1,10 @@
 import sys
 import time
 import logging
+import subprocess
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QProcess
+from PyQt5.QtCore import QThread, QObject, pyqtSignal, pyqtSlot
 
 try:
 	from ui.ui_main import Ui_MainWindow
@@ -13,54 +14,55 @@ except ModuleNotFoundError:
 # logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.DEBUG)
 
+
+class Worker(QObject):
+	finished = pyqtSignal()
+	intReady = pyqtSignal(str)
+
+	def start_flake_process(self):
+		try:
+			p = subprocess.check_output(['flake8', '--ignore', 'W191,E501,E266,E402,F403,E261,F405', '/home/tabulate/flakewatcher/flakewatcher/main.py']).decode('utf-8')
+		except Exception as e:
+			p = str(e.output.decode('utf-8'))
+
+		logging.debug(f'{p=}')
+		return p
+
+	@pyqtSlot()
+	def procCounter(self): # A slot takes no params
+		while True:
+			self.intReady.emit(self.start_flake_process())
+			time.sleep(2)
+
+		self.finished.emit()
+
+
 class FlakeWatcherMain(QMainWindow, Ui_MainWindow):
 	def __init__(self, *args, obj=None, **kwargs):
 		super(FlakeWatcherMain, self).__init__(*args, **kwargs)
 
 		self.process = None
 
+		self.obj = Worker()
+		self.thread = QThread()
+		self.obj.intReady.connect(self.update_label)
+		self.obj.moveToThread(self.thread)
+		self.obj.finished.connect(self.thread.quit)
+		self.thread.started.connect(self.obj.procCounter)
+		self.thread.start()
+
 		self.setupUi(self)
 
-		self.StartButton.clicked.connect(self.start_flake_process)
+	def update_label(self, text):
+		self.FlakeOutput.setText('{}'.format(text))
 
-	def start_flake_process(self):
-		if self.process is None: # No process running
-			self.process = QProcess()
-			self.process.readyReadStandardError.connect(self.handle_process_stderr)
-			self.process.readyReadStandardOutput.connect(self.update_flake_output_text)
-			self.process.stateChanged.connect(self.handle_state)
-			self.process.finished.connect(self.process_finished)
-			self.process.start('flake8', ['--ignore', 'W191,E501,E266,E402,F403,E261,F405', '/home/tabulate/TabulateCalc/main.py'])
-			logging.debug('process started')
-
-	def process_finished(self):
-		self.process = None
-		logging.debug('Process removed')
-
-	def update_flake_output_text(self):
-		logging.debug('stdout')
-		data = self.process.readAllStandardOutput().data().decode()
-		logging.debug(f"{data=}")
-		self.FlakeOutput.setText(data)
-
-	def handle_state(self, state):
-		states = {
-			QProcess.NotRunning: 'Not running',
-			QProcess.Starting: 'Starting',
-			QProcess.Running: 'Running',
-		}
-		state_name = states[state]
-		logging.debug(f"State changed: {state_name}")
-
-	def handle_process_stderr(self):
-		err = self.process.readAllStandardError().data().decode().strip()
-		logging.warning(err)
 
 def startQtApp():
 	app = QApplication(sys.argv)
 	window = FlakeWatcherMain()
 	window.show()
 	sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
 	startQtApp()
